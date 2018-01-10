ad5206652d0eff6b323077076fcf2ee464d937b5
Don Mohlmaster <dmohlmaster@tervis.com>
2018-01-10 14:47:37 -0500

    Fix for template names

--- TervisRemoteDesktopManager.psm1
+++ TervisRemoteDesktopManager.psm1
@@ -16,12 +16,12 @@ function New-TervisRDMNodeSession {
         [parameter(Mandatory,ValueFromPipelineByPropertyName)]$EnvironmentName,
         [parameter(Mandatory,ValueFromPipelineByPropertyName)][ValidateSet("Windows Server 2016","CentOS","Linux")]$TemplateName
     )
-    if ($OS -eq "Windows Server 2016"){
+    if ($TemplateName -eq "Windows Server 2016"){
         $RDMTemplate = Get-RDMTemplate | where name -eq "Windows RDP"
         $SessionType = "RDPConfigured"
     }
-    else {
-        $RDMTemplate = Get-RDMTemplate | where name -eq "Linux Standard"
+    elseif (($TemplateName -eq "CentOS") -or ($TemplateName -eq "Linux")) {
+        $RDMTemplate = Get-RDMTemplate | where-object name -eq "Linux Standard"
         $SessionType = "Putty"
     }
     $RDMSession = New-RDMSession -TemplateID $RDMTemplate.ID -Host $Computername -Name $Computername -Type $SessionType -Group $EnvironmentName
